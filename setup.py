6a47aaf1b053438e8ed5c5b8ac88f146ca535ab0
Liam Hays <liamrhays@gmail.com>
2022-04-02 14:23:00 -0600

    Added dependencies to setup.py.

--- setup.py
+++ setup.py
@@ -15,12 +15,41 @@ setuptools.setup(
     #project_urls={
     #    "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
     #},
-    #classifiers=[
-    #    "Programming Language :: Python :: 3",
-    #    "License :: OSI Approved :: MIT License",
-    #    "Operating System :: OS Independent",
-    #],
+    
+    # Do we really need these? No. I have no plans to put HPex on
+    # PyPI, which is what the classifiers are for. However, it might
+    # help somebody trying to categorize HPex.
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)", 
+        "Operating System :: POSIX :: Linux",
+        "Natural Language :: English",
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: End Users/Desktop",
+        "Topic :: Utilities"
+    ],
+
+
+    # HPex can be run with 'python -m hpex' or just 'hpex' in the
+    # shell. It seems that the best way to do this is to keep the main
+    # HPex dispatcher in __main__.py, then entry_points can reference
+    # and instantiate that object.
+    entry_points={
+        'console_scripts': [
+            'hpex=hpex.__main__:HPex'
+        ]
+    }, 
     packages=['hpex'], 
     package_dir={"": "src"},
     python_requires=">=3.9",
+    install_requires=[
+        'wxPython',
+        'xmodem',
+        'pyserial',
+        'PyPubSub',
+        'ptyprocess'
+    ]
+    
+        
+        
 )
